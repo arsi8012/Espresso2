116a89b8e68a2a0bb7f041dac09292cdf4878c95
Олег А. Атласов <atlasovoa@kchadm.ru>
2022-05-04 15:21:19 +0400

    Added tests espresso

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/EspressoTest.java
@@ -0,0 +1,89 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.CoreMatchers.allOf;
+
+import android.content.Intent;
+
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class EspressoTest {
+
+    @Before
+    public void registerIdlingResources() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResources.idlingResource);
+    }
+
+    @After
+    public void unregisterIdlingResources() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResources.idlingResource);
+    }
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testName() {
+        ViewInteraction mainText = onView(
+                withId(R.id.text_home)
+        );
+        mainText.check(
+                matches(
+                        withText("This is home fragment")
+                )
+        );
+    }
+
+    @Test
+    public void testOpenRightMenu() {
+        ViewInteraction element = onView(
+                withContentDescription("More options")
+        );
+        element.perform(click());
+        ViewInteraction elementId = onView(
+                withId(R.id.title)
+        );
+        elementId.check(matches
+                (withText("Settings")
+                )
+        );
+        Intents.init();
+        elementId.perform(click());
+        intended(hasData("https://google.com"));
+        intended(hasAction(Intent.ACTION_VIEW));
+        Intents.release();
+    }
+
+    @Test
+    public void testOpenGallery() {
+        ViewInteraction element = onView(
+                withContentDescription("Open navigation drawer")
+        );
+        element.perform(click());
+        ViewInteraction gallery = onView(withText("Gallery"));
+        gallery.perform(click());
+        ViewInteraction itemSeven = onView(allOf(withId(R.id.item_number), withText("7")));
+        itemSeven.check(matches(withText("7")));
+    }
+}
